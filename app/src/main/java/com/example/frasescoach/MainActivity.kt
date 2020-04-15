0be984393bdbc2396346665e25a2b0dcd9688b2a
Bevilaqua, Bruno <bruno.infobr@gmail.com>
2020-04-15 01:20:13 -0300

    Adicionando suporte a Talkback para frase coach

--- app/src/main/java/com/example/frasescoach/MainActivity.kt
+++ app/src/main/java/com/example/frasescoach/MainActivity.kt
@@ -9,21 +9,22 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-    btnFrase.setOnClickListener(View.OnClickListener {
-     cliqueBotao(btnFrase)
-    })
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnFrase.setOnClickListener(View.OnClickListener {
+            cliqueBotao(btnFrase)
+        })
 
-  }
+    }
 
 
-  fun cliqueBotao(view: View?){
-    val frases  = resources.getTextArray(R.array.frases)
-    var sortear = Random.nextInt(0,frases.size-1);
-    Log.i("SORTEIO","Numero sorteado $sortear")
-    txtFrase.text=   frases[sortear]
-    Log.i("SORTEIO","Frase $frases")
-  }
+    fun cliqueBotao(view: View?) {
+        val frases = resources.getTextArray(R.array.frases)
+        var sortear = Random.nextInt(0, frases.size - 1);
+        Log.i("SORTEIO", "Numero sorteado $sortear")
+        txtFrase.text = frases[sortear]
+        txtFrase.contentDescription = frases[sortear]
+        Log.i("SORTEIO", "Frase $frases")
+    }
 }
